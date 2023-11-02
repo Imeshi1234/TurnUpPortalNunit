1f973c2e7e422b013722f6678cb95e98a6f2c6cd
Anuttara Kumarapperuma <anuttara1989@gmail.com>
2023-11-02 18:44:59 +1300

    Added common methods with wait and separate tests using Nunit implementation

---
+++ TurnUpPortalNunit/Test/TMTest.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnUpPortalNunit.pages;
+using NUnit.Framework;
+using TurnUpPortalNunit.Utilities;
+
+
+namespace TurnUpPortalNunit.Test
+{
+
+    [TestFixture]
+    public class TMTest:CommonDriver
+    {
+        [SetUp]
+        public void SetUpTM()
+        {
+            //Open chrome browser
+            driver = new ChromeDriver();
+
+            //LoginPage page object initialization and definition
+            LoginPage loginObj = new LoginPage();
+            loginObj.LoginActions(driver);
+
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            //HomePage page object initialization and definition
+            HomePage homepageObj = new HomePage();
+            homepageObj.VerifySucessLogin(driver);
+            homepageObj.GoToTMPage(driver);
+
+        }
+        [Test,Order(1),Description("This test create new Time Record with valid data")]
+        public void TestCreateTimeRecord()
+        {
+            TMPage tmpageObj = new TMPage();
+            tmpageObj.CreateTimeRecord(driver);
+        }
+        [Test, Order(2), Description("This test update an existing Time Record with valid data")]
+        public void TestEditTimeRecord()
+        {
+            TMPage tmpageObj = new TMPage();
+            tmpageObj.EditTimeRecord(driver);
+        }
+        [Test, Order(3), Description("This test delete an existing Time Record ")]
+        public void TestDeleteTimeRecord()
+        {
+            TMPage tmpageObj = new TMPage();
+            tmpageObj.DeleteTimeRecord(driver);
+        }
+
+
+        [TearDown]
+        public void CloseTestRun()
+        {
+            driver.Quit();
+
+        }
+    }
+}
+
+  
+
+
+        
---
+++ TurnUpPortalNunit/Utilities/Wait.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnUpPortalNunit.Utilities
+{
+    public class Wait
+    {
+        //if you use static we dont wangt to make objects to call the methods, we can directly call it.
+        public static void WaitToBeClickable(IWebDriver driver, string locatorType, string locatorValue, int seconds)
+
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+            //to intrduce multiple contions
+            if (locatorValue == "Xpath")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(locatorValue)));
+            }
+            if (locatorValue == "Id")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(locatorValue)));
+            }
+            if (locatorValue == "CssSelector")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector(locatorValue)));
+            }
+            if (locatorValue == "Name")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Name(locatorValue)));
+            }
+        }
+
+        public static void WaitToBeVisible(IWebDriver driver, string locatorType, string locatorValue, int seconds)
+
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+            //to intrduce multiple contions
+            if (locatorValue == "Xpath")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(locatorValue)));
+            }
+            if (locatorValue == "Id")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id(locatorValue)));
+            }
+            if (locatorValue == "CssSelector")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector(locatorValue)));
+            }
+            if (locatorValue == "Name")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Name(locatorValue)));
+            }
+        }
+    } 
+}
